1bad3b3a989b14e9cdaa53091cae425a12bfa841
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-07-04 20:47:19 +0900

    fixed typo

--- test/sub.f90
+++ test/sub.f90
@@ -2,26 +2,27 @@ program test_sub
     use, intrinsic :: iso_fortran_env
     use :: strith_parameter
     use :: strith_arithmetic_basic_core_sub
+    use :: strith_arithmetic_basic_sub
     implicit none
 
-    character(len=digits) :: a, b, add
+    character(len=digits) :: a, b, sub
     character(:), allocatable :: expr
 
-    add = sub_core(weights_of_digits(4), weights_of_digits(3))
+    sub = sub_core(weights_of_digits(4), weights_of_digits(3))
     expr = "16-8=8"
-    if (add == "+000000000000000000000000000000000000008") then
+    if (sub == "+000000000000000000000000000000000000008") then
         print *, "PASSED: ", expr
     else
-        print *, "FAILED: expected "//expr//", but got ", add
+        print *, "FAILED: expected "//expr//", but got ", sub
     end if
 
     a = "+000000000000000000000000152125131763605"
     b = "+000000000000000000000000007420738134810"
-    add = sub_core(a, b)
+    sub = sub_core(a, b)
     expr = "152125131763605+7420738134810=144704393628795"
-    if (add == "+000000000000000000000000144704393628795") then
+    if (sub == "+000000000000000000000000144704393628795") then
         print *, "PASSED: ", expr
     else
-        print *, "FAILED: expected "//expr//", but got ", add
+        print *, "FAILED: expected "//expr//", but got ", sub
     end if
 end program test_sub
