f9d4943bf6a140fa1b80cdf81b35854db9e20e69
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-07-04 13:07:17 +0900

    changed parameter name

--- src/parameter/parameter.f90
+++ src/parameter/parameter.f90
@@ -146,7 +146,7 @@ module strith_parameter
         !! weights of corresponding digits
     integer(int32), public, parameter :: digits = len(weights_of_digits(0))
         !! length of a string
-    integer(int32), public, parameter :: most_significant_digit_index = 2
+    integer(int32), public, parameter :: leftmost_digit_index = 2
         !! the leftmost position of a number in a string
     integer(int32), public, parameter :: rightmost_digit_index = digits
         !! the rightmost position of a number in a string
