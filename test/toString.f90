9b6b527dca0192d935cd4579d9a5c4f13eebfdea
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-07-06 00:45:01 +0900

    updated tests for to_string

--- test/toString.f90
+++ test/toString.f90
@@ -5,6 +5,7 @@ program test_toString
 
     character(:), allocatable :: str
 
+    !--- naive
     str = to_string(huge(0_int8))
     if (str == "+000000000000000000000000000000000000127") then
         print *, "PASSED: to_string for int8"
@@ -13,12 +14,10 @@ program test_toString
     end if
 
     str = to_string(int(B"11111111", kind=int8))
-    if (str == "+000000000000000000000000000000000000255") then
-        print *, "EXPECTED FAILURE: to_string for int8 currently cannot convert negative values."
-    else if (str == "-000000000000000000000000000000000000128") then
-        print *, "UNEXPECTED PASS: to_string for int8"
+    if (str == "-000000000000000000000000000000000000001") then
+        print *, "PASSED: to_string for negative int8"
     else
-        print *, "FAILED: to_string for int8. expected +000000000000000000000000000000000000255 but got"//str
+        print *, "FAILED: to_string for negative int8. expected -000000000000000000000000000000000000001 but got"//str
     end if
 
     str = to_string(huge(0_int16))
@@ -28,11 +27,25 @@ program test_toString
         print *, "FAILED: to_string for int16. expected +000000000000000000000000000000000032767 but got"//str
     end if
 
+    str = to_string(int(B"1111111111111111", kind=int16))
+    if (str == "-000000000000000000000000000000000000001") then
+        print *, "PASSED: to_string for negative int16"
+    else
+        print *, "FAILED: to_string for negative int16. expected -000000000000000000000000000000000000001 but got"//str
+    end if
+
     str = to_string(huge(0_int32))
     if (str == "+000000000000000000000000000002147483647") then
         print *, "PASSED: to_string for int32"
     else
         print *, "FAILED: to_string for int32. expected +000000000000000000000000000002147483647 but got"//str
+    end if
+
+    str = to_string(int(B"11111111111111111111111111111111", kind=int32))
+    if (str == "-000000000000000000000000000000000000001") then
+        print *, "PASSED: to_string for negative int32."
+    else
+        print *, "FAILED: to_string for negative int32. expected -000000000000000000000000000000000000001 but got"//str
     end if
 
     str = to_string(huge(0_int64))
@@ -42,6 +55,14 @@ program test_toString
         print *, "FAILED: to_string for int64. expected +000000000000000000009223372036854775807 but got"//str
     end if
 
+    str = to_string(int(B"1111111111111111111111111111111111111111111111111111111111111111", kind=int64))
+    if (str == "-000000000000000000000000000000000000001") then
+        print *, "PASSED: to_string for negative int64."
+    else
+        print *, "FAILED: to_string for negative int64. expected -000000000000000000000000000000000000001 but got"//str
+    end if
+
+    !--- remove 0 padding
     str = to_string(huge(0_int8), remove_0_padding=.true.)
     if (str == "127") then
         print *, "PASSED: to_string for int8 with remove 0 padding"
@@ -50,12 +71,10 @@ program test_toString
     end if
 
     str = to_string(int(B"11111111", kind=int8), remove_0_padding=.true.)
-    if (str == "255") then
-        print *, "EXPECTED FAILURE: to_string for int8 currently cannot convert negative values."
-    else if (str == "-128") then
-        print *, "UNEXPECTED PASS: to_string for int8"
+    if (str == "-1") then
+        print *, "PASSED: to_string for negative int8 with remove 0 padding"
     else
-        print *, "FAILED: to_string for int8. expected 255 but got"//str
+        print *, "FAILED: to_string for negative int8 with remove 0 padding. expected -1 but got"//str
     end if
 
     str = to_string(huge(0_int16), remove_0_padding=.true.)
@@ -65,6 +84,13 @@ program test_toString
         print *, "FAILED: to_string for int16 with remove 0 padding. expected 32767 but got"//str
     end if
 
+    str = to_string(int(B"1111111111111111", kind=int16), remove_0_padding=.true.)
+    if (str == "-1") then
+        print *, "PASSED: to_string for negative int16 with remove 0 padding"
+    else
+        print *, "FAILED: to_string for negative int16 with remove 0 padding. expected -1 but got"//str
+    end if
+
     str = to_string(huge(0_int32), remove_0_padding=.true.)
     if (str == "2147483647") then
         print *, "PASSED: to_string for int32 with remove 0 padding"
@@ -72,10 +98,85 @@ program test_toString
         print *, "FAILED: to_string for int32 with remove 0 padding. expected 2147483647 but got"//str
     end if
 
+    str = to_string(int(B"11111111111111111111111111111111", kind=int32), remove_0_padding=.true.)
+    if (str == "-1") then
+        print *, "PASSED: to_string for negative int32 with remove 0 padding"
+    else
+        print *, "FAILED: to_string for negative int32 with remove 0 padding. expected -1 but got"//str
+    end if
+
     str = to_string(huge(0_int64), remove_0_padding=.true.)
     if (str == "9223372036854775807") then
         print *, "PASSED: to_string for int64 with remove 0 padding"
     else
         print *, "FAILED: to_string for int64 with remove 0 padding. expected 9223372036854775807 but got"//str
+    end if
+
+    str = to_string(int(B"1111111111111111111111111111111111111111111111111111111111111111", kind=int64), remove_0_padding=.true.)
+    if (str == "-1") then
+        print *, "PASSED: to_string for negative int64 with remove 0 padding"
+    else
+        print *, "FAILED: to_string for negative int64 with remove 0 padding. expected -1 but got"//str
+    end if
+
+    !--- as unsigned
+    str = to_string(int(B"11111111", kind=int8), as_unsigned=.true.)
+    if (str == "+000000000000000000000000000000000000255") then
+        print *, "PASSED: to_string for unsigned int8"
+    else
+        print *, "FAILED: to_string for unsigned int8. "// &
+            "expected +000000000000000000000000000000000000255 but got"//str
+    end if
+
+    str = to_string(int(B"1111111111111111", kind=int16), as_unsigned=.true.)
+    if (str == "+000000000000000000000000000000000065535") then
+        print *, "PASSED: to_string for unsigned int16"
+    else
+        print *, "FAILED: to_string for unsigned int16. expected +000000000000000000000000000000000065535 but got"//str
+    end if
+
+    str = to_string(int(B"11111111111111111111111111111111", kind=int32), as_unsigned=.true.)
+    if (str == "+000000000000000000000000000004294967295") then
+        print *, "PASSED: to_string for unsigned int32"
+    else
+        print *, "FAILED: to_string for unsigned int32. expected +000000000000000000000000000004294967295 but got"//str
+    end if
+
+    str = to_string(int(B"1111111111111111111111111111111111111111111111111111111111111111", kind=int64), as_unsigned=.true.)
+    if (str == "+000000000000000000018446744073709551615") then
+        print *, "PASSED: to_string for unsigned int64"
+    else
+        print *, "FAILED: to_string for unsigned int64. expected +000000000000000000018446744073709551615 but got"//str
+    end if
+
+    !--- remove 0 padding and as unsigned
+    str = to_string(int(B"11111111", kind=int8), remove_0_padding=.true., as_unsigned=.true.)
+    if (str == "255") then
+        print *, "PASSED: to_string for unsigned int8 with remove 0 padding"
+    else
+        print *, "FAILED: to_string for unsigned int8 with remove 0 padding. "// &
+            "expected 255 but got"//str
+    end if
+
+    str = to_string(int(B"1111111111111111", kind=int16), remove_0_padding=.true., as_unsigned=.true.)
+    if (str == "65535") then
+        print *, "PASSED: to_string for unsigned int16 with remove 0 padding"
+    else
+        print *, "FAILED: to_string for unsigned int16 with remove 0 padding. expected 65535 but got"//str
+    end if
+
+    str = to_string(int(B"11111111111111111111111111111111", kind=int32), remove_0_padding=.true., as_unsigned=.true.)
+    if (str == "4294967295") then
+        print *, "PASSED: to_string for unsigned int32 with remove 0 padding"
+    else
+        print *, "FAILED: to_string for unsigned int32 with remove 0 padding. expected 4294967295 but got"//str
+    end if
+
+    str = to_string(int(B"1111111111111111111111111111111111111111111111111111111111111111", kind=int64), &
+                    remove_0_padding=.true., as_unsigned=.true.)
+    if (str == "18446744073709551615") then
+        print *, "PASSED: to_string for unsigned int64 with remove 0 padding"
+    else
+        print *, "FAILED: to_string for unsigned int64 with remove 0 padding. expected 18446744073709551615 but got"//str
     end if
 end program test_toString
