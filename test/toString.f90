286d3325fa02095c7db2a84b69db145ae0317754
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-07-05 23:59:22 +0900

    fixed assertion message

--- test/toString.f90
+++ test/toString.f90
@@ -44,9 +44,9 @@ program test_toString
 
     str = to_string(huge(0_int8), remove_0_padding=.true.)
     if (str == "127") then
-        print *, "PASSED: to_string for int8 with remove heading 0"
+        print *, "PASSED: to_string for int8 with remove 0 padding"
     else
-        print *, "FAILED: to_string for int8 with remove heading 0. expected 127 but got"//str
+        print *, "FAILED: to_string for int8 with remove 0 padding. expected 127 but got"//str
     end if
 
     str = to_string(int(B"11111111", kind=int8), remove_0_padding=.true.)
@@ -60,22 +60,22 @@ program test_toString
 
     str = to_string(huge(0_int16), remove_0_padding=.true.)
     if (str == "32767") then
-        print *, "PASSED: to_string for int16 with remove heading 0"
+        print *, "PASSED: to_string for int16 with remove 0 padding"
     else
-        print *, "FAILED: to_string for int16 with remove heading 0. expected 32767 but got"//str
+        print *, "FAILED: to_string for int16 with remove 0 padding. expected 32767 but got"//str
     end if
 
     str = to_string(huge(0_int32), remove_0_padding=.true.)
     if (str == "2147483647") then
-        print *, "PASSED: to_string for int32 with remove heading 0"
+        print *, "PASSED: to_string for int32 with remove 0 padding"
     else
-        print *, "FAILED: to_string for int32 with remove heading 0. expected 2147483647 but got"//str
+        print *, "FAILED: to_string for int32 with remove 0 padding. expected 2147483647 but got"//str
     end if
 
     str = to_string(huge(0_int64), remove_0_padding=.true.)
     if (str == "9223372036854775807") then
-        print *, "PASSED: to_string for int64 with remove heading 0"
+        print *, "PASSED: to_string for int64 with remove 0 padding"
     else
-        print *, "FAILED: to_string for int64 with remove heading 0. expected 9223372036854775807 but got"//str
+        print *, "FAILED: to_string for int64 with remove 0 padding. expected 9223372036854775807 but got"//str
     end if
 end program test_toString
