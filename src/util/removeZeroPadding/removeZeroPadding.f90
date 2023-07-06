2f4463025e1d13e0dc634296c71db6ece3817263
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-07-06 14:08:31 +0900

    fixed to consider that a == zero and == -zero

--- src/util/removeZeroPadding/removeZeroPadding.f90
+++ src/util/removeZeroPadding/removeZeroPadding.f90
@@ -1,6 +1,8 @@
 module strith_util_removeZeroPadding
     use, intrinsic :: iso_fortran_env
     use :: strith_parameter
+    use :: strith_comparision_equal
+    use :: strith_arithmetic_unary_negate
     implicit none
     private
     public :: remove_zero_padding
@@ -12,6 +14,16 @@ contains
         character(len=digits) :: a_wo_0
 
         integer(int32) :: pos_0
+
+        if (a.stritheq.zero) then
+            a_wo_0 = "0"
+            return
+        end if
+
+        if (a.stritheq.-zero) then
+            a_wo_0 = "-0"
+            return
+        end if
 
         a_wo_0 = a
         pos_0 = index(a_wo_0, "0")
