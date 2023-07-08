fdd99e655b97aabb2574d2d6e3aef6665621e7c5
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-07-09 00:24:45 +0900

    refactored `is_valid` procedure

--- src/util/isValid/isValid.f90
+++ src/util/isValid/isValid.f90
@@ -11,8 +11,16 @@ contains
         character(len=digits), intent(in) :: a
 
         is_valid = all([ &
-                       verify(a(sign_index:sign_index), sign_set) == 0, &
-                       verify(a(leftmost_digit_index:rightmost_digit_index), number_set) == 0 &
+                       is_contained(a(sign_index:sign_index), sign_set), &
+                       is_contained(a(leftmost_digit_index:rightmost_digit_index), number_set) &
                        ])
     end function is_valid
+
+    logical function is_contained(a, set_of_characters)
+        implicit none
+        character(*), intent(in) :: a
+        character(*), intent(in) :: set_of_characters
+
+        is_contained = (verify(a, set_of_characters) == 0)
+    end function is_contained
 end module strith_util_isValid
